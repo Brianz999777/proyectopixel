ec91504e885d458cd24f777bf7ae40a5e8422633
brianzubuntu <bkpr9000@gmail.com>
2026-02-12 03:28:21 +0100

    regreso a 3000

--- app/index.js
+++ app/index.js
@@ -2,4 +2,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.end('¡Hola! App desplegada con Ansible y GitHub Actions');
 });
-server.listen(80, () => console.log('Servidor listo en puerto 80'));
+server.listen(3000, () => console.log('Servidor listo en puerto 3000'));
