dcf8065855ba0daf3a01b236695c22af9ad8ad70
brianzpixel <brianz999777@gmail.com>
2026-02-26 02:33:47 +0100

    link de tunel

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('¡Hola! App desplegada con Ansible y GitHub Actions');
+  res.end('¡Hola! App desplegada con Ansible y GitHub Actions, pixel');
 });
 server.listen(3000, () => console.log('Servidor listo en puerto 3000'));
